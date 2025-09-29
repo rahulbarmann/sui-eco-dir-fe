7ecf5e957ffd1cb385acf23a2a22b23e76be629d
rahulbarman <itsrahulbarman1@gmail.com>
2025-09-29 14:01:29 +0530

    chore: cleanup

--- src/routes/ProjectPage/screens/ProjectPage.tsx
+++ src/routes/ProjectPage/screens/ProjectPage.tsx
@@ -291,7 +291,7 @@ export const ProjectPage = (): JSX.Element => {
             </div>
 
             {/* Dashboard Screenshots Section */}
-            <section className="relative px-4 sm:px-6 lg:px-10 py-8 mt-16 sm:mt-20 lg:mt-24 z-80">
+            <section className="relative px-4 sm:px-6 lg:px-10 py-8 mt-8 sm:mt-12 lg:mt-16 z-80">
                 <div className="max-w-7xl mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                         {dashboardScreenshots.map((screenshot, index) => (
